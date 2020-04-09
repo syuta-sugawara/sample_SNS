80e6695ced52e855c1d2a192ed521a0c80983a3d
junkisai <jonki.beam@gmail.com>
2020-04-09 17:03:06 +0900

    wip: サンプルコード

--- client/src/stories/Button.stories.tsx
+++ client/src/stories/Button.stories.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from '../components/Button';
 
-storiesOf('Button', module).add('with text', () => {
-  return <Button text="Click Me" />;
-});
+storiesOf('Button', module)
+  .add('blue', () => {
+    return <Button text="Click Me" color="blue" />;
+  })
+  .add('red', () => {
+    return <Button text="Click Me" color="red" />;
+  });

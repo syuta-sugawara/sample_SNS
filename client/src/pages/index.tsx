2edeac7d7a1332f56dfe1bc92941249fb5fa9593
junkisai <jonki.beam@gmail.com>
2020-04-09 20:11:38 +0900

    wip: useDispatch sample

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NextPage } from 'next';
 
+import { incrementAction } from '../store/counter/acitons';
+import { CounterActionTypes } from '../store/counter/types';
 import { RootState } from '../store';
 
 const Index: NextPage = () => {
+  const dispatch = useDispatch();
   const currentCount = useSelector((state: RootState) => state.counter);
   console.log(currentCount);
-  return <h1>Hello world!</h1>;
+
+  const handleIncrement = (): CounterActionTypes => dispatch(incrementAction());
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <button onClick={handleIncrement}>+1</button>
+    </>
+  );
 };
 
 export default Index;

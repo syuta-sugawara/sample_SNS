9542563495265fe6be219f00f66cb904318b4c27
田中俊太朗 <tanaka_shuntaro@applibot.co.jp>
2020-04-10 14:03:33 +0900

    キャメルに変更

--- backend/api-server/controller/users_controller.go
+++ backend/api-server/controller/users_controller.go
@@ -20,30 +20,30 @@ func NewUserController(db *dynamo.DB) UsersController {
 
 // ユーザー情報の取得
 func (uc *UsersController) UserIndex(c echo.Context) error {
-    user_id := c.Param("userName")    
+    userID := c.Param("userName")  
     uc.userModel.All()
-    return c.String(http.StatusOK, "GetUserInfo" + user_id)
+    return c.String(http.StatusOK, "GetUserInfo" + userID)
 }
 
 // フォローの取得
 func (uc *UsersController) FollowsIndex(c echo.Context) error {
-    user_id := c.Param("userName")
+    userID := c.Param("userName")
     uc.userModel.All()
-    return c.String(http.StatusOK, "GetFollows" + user_id)
+    return c.String(http.StatusOK, "GetFollows" + userID)
 }
 
 // フォロワーの取得
 func (uc *UsersController) FollowersIndex(c echo.Context) error {
-    user_id := c.Param("userName")
+    userID := c.Param("userName")
     uc.userModel.All()
-    return c.String(http.StatusOK, "GetFollowers" + user_id)
+    return c.String(http.StatusOK, "GetFollowers" + userID)
 }
 
 // ユーザー情報更新
 func (uc *UsersController) UpdateUser(c echo.Context) error {
-    user_id := c.Param("userName")
+    userID := c.Param("userName")
     uc.userModel.All()
-    return c.String(http.StatusOK, "GetFollowers" + user_id)
+    return c.String(http.StatusOK, "GetFollowers" + userID)
 }
 
 // ユーザー登録
@@ -54,14 +54,14 @@ func (uc *UsersController) RegisterUser(c echo.Context) error {
 
 // フォロー処理
 func (uc *UsersController) Follow(c echo.Context) error {
-    user_id := c.Param("userName")
+    userID := c.Param("userName")
     uc.userModel.All()
-    return c.String(http.StatusOK, "Follow" + user_id)
+    return c.String(http.StatusOK, "Follow" + userID)
 }
 
 // アンフォロー処理
 func (uc *UsersController) Unfollow(c echo.Context) error {
-    user_id := c.Param("userName")
+    userID := c.Param("userName")
     uc.userModel.All()
-    return c.String(http.StatusOK, "Unfollow" + user_id)
+    return c.String(http.StatusOK, "Unfollow" + userID)
 }

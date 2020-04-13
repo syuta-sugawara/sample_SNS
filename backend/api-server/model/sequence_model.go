629af89ff8ae3b135ab05d1f7172eabd9b389d7e
田中俊太朗 <syuntaro28x@gmail.com>
2020-04-13 12:30:15 +0900

    NotFound判別処理追加

--- backend/api-server/model/sequence_model.go
+++ backend/api-server/model/sequence_model.go
@@ -21,9 +21,10 @@ func (sm *SequenceModel) NextID(table string) int {
 	s := new(entity.Sequence)
 	if err := sm.sequenceTable.Get("table", table).One(s); err != nil {
 		fmt.Println(err)
-		fmt.Println("create sequence")
-		sm.sequenceTable.Update("table", table).Set("currentNumber", 1).Value(s)
-		return s.CurrentNumber
+		if IsNotFound(err) {
+			sm.sequenceTable.Update("table", table).Set("currentNumber", 1).Value(s)
+			return s.CurrentNumber
+		}
 	}
 
 	if err := sm.sequenceTable.Update("table", table).Set("currentNumber", s.CurrentNumber+1).Value(s); err != nil {

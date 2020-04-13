88ab99166d41b6f6045599b58586691e6baad38e
syuta sugawara <syuta.sugawara@gmail.com>
2020-04-13 19:23:48 +0900

    feat: creating nav

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -3,20 +3,41 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Button, { Variant } from './Button';
+import TwitterIcon from './icons/TwitterIcon';
+import styled from 'styled-components';
+
+const StyledNav = styled.div`
+  max-width: 200px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+const ButtonContainer = styled.div`
+  padding: 10px;
+`;
+
+const Icon = styled(TwitterIcon)`
+  width: 50px;
+  height: 50px;
+  fill: #fff;
+  stroke: #000;
+`;
 
 const Navigation: React.FC = () => {
   return (
-    <List>
-      <ListItem button>
-        <ListItemText>ホーム</ListItemText>
-      </ListItem>
-      <ListItem button>
-        <ListItemText>プロフィール</ListItemText>
-      </ListItem>
-      <ListItem button>
+    <StyledNav>
+      <Icon />
+      <List>
+        <ListItem button>
+          <ListItemText>ホーム</ListItemText>
+        </ListItem>
+        <ListItem button>
+          <ListItemText>プロフィール</ListItemText>
+        </ListItem>
+      </List>
+      <ButtonContainer>
         <Button text="ツイートする" variant={Variant.CONTAINED} />
-      </ListItem>
-    </List>
+      </ButtonContainer>
+    </StyledNav>
   );
 };
 

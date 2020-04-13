0e6aa198bb7fc015488f6d57504982a410bfca5f
syuta sugawara <syuta.sugawara@gmail.com>
2020-04-13 14:08:35 +0900

    feat: creating a navigation component

---
+++ client/src/components/Navigation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+import STYLES from "../styles/const";
+
+const Navigation = () => {
+  return <div>hello world</div>;
+};
+
+export default Navigation;

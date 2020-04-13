1ca626093f144cd7126bfce839e586fa0409183c
syuta sugawara <syuta.sugawara@gmail.com>
2020-04-13 16:51:06 +0900

    feat: creating navigation menu

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button, { Variant } from './Button';
 
 const Navigation: React.FC = () => {
-  return <div>hello world</div>;
+  return (
+    <List>
+      <ListItem button>
+        <ListItemText>ホーム</ListItemText>
+      </ListItem>
+      <ListItem button>
+        <ListItemText>プロフィール</ListItemText>
+      </ListItem>
+      <ListItem button>
+        <Button text="ツイートする" variant={Variant.CONTAINED} disabled />
+      </ListItem>
+    </List>
+  );
 };
 
 export default Navigation;

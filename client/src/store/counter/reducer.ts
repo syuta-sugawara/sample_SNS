8db616ca4fd8d7002a08f1703f2683bfa9c439d3
junkisai <jonki.beam@gmail.com>
2020-04-14 15:43:20 +0900

    feat: apiつなぎこみサンプル

--- client/src/store/counter/reducer.ts
+++ client/src/store/counter/reducer.ts
@@ -4,10 +4,16 @@ import counterAction from './acitons';
 
 export type CountType = {
   value: number;
+  results: {};
+  loading: boolean;
+  error: any;
 };
 
 const initialState: CountType = {
   value: 0,
+  results: [],
+  loading: false,
+  error: null,
 };
 
 const countReducer = reducerWithInitialState(initialState)
@@ -18,6 +24,24 @@ const countReducer = reducerWithInitialState(initialState)
   .case(counterAction.decrement, (state, payload) => ({
     ...state,
     value: state.value - payload.count,
+  }))
+  .case(counterAction.getAllDocs.started, state => ({
+    ...state,
+    results: [],
+    loading: true,
+    error: null,
+  }))
+  .case(counterAction.getAllDocs.done, (state, payload) => ({
+    ...state,
+    results: payload.result,
+    loading: false,
+    error: null,
+  }))
+  .case(counterAction.getAllDocs.failed, (state, payload) => ({
+    ...state,
+    results: [],
+    loading: false,
+    error: payload.error,
   }));
 
 export default countReducer;

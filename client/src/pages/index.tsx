b2328812ef61a8097a9a28ba5fb30304f72ef31c
junkisai <jonki.beam@gmail.com>
2020-04-14 18:54:30 +0900

    fix: delete sample code

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { NextPage } from 'next';
 
-import counterAction, { fetchDocs } from '../store/counter/acitons';
-import { RootState } from '../store';
-
 const Index: NextPage = () => {
-  const dispatch = useDispatch();
-  const currentCount = useSelector((state: RootState) => state.counter);
-  console.log(currentCount);
-
-  const handleIncrement = (): any => {
-    dispatch(counterAction.increment({ count: 1 }));
-  };
-
   return (
     <>
       <h1>Hello world!</h1>
-      <button onClick={handleIncrement}>+1</button>
     </>
   );
-};
-
-Index.getInitialProps = async ({ store }): Promise<any> => {
-  await store.dispatch(fetchDocs());
 };
 
 export default Index;

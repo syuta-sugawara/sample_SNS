cc91056a6a8d4f1a29bf041ed9bdfb2faada2418
junkisai <jonki.beam@gmail.com>
2020-04-15 13:06:17 +0900

    feat: postTweet

--- client/src/requests/tweet.ts
+++ client/src/requests/tweet.ts
@@ -1,7 +1,19 @@
 import fetch from 'isomorphic-unfetch';
 
+import { PostTweetType } from '../types/tweet';
+
 export default class TweetAPI {
   getAllTweets = () => {
     return fetch(`${process.env.API_URL}/tweets`);
+  };
+
+  postTweet = (data: PostTweetType) => {
+    return fetch(`${process.env.API_URL}/tweets`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(data),
+    });
   };
 }
--- client/src/store/tweet/actions.ts
+++ client/src/store/tweet/actions.ts
@@ -3,8 +3,9 @@ import { Dispatch } from 'redux';
 
 import TweetAPI from '../../requests/tweet';
 import { ActionTypes } from '../actionTypes';
-import { TweetType } from '../../types/tweet';
-import { ErrorRespose } from '../../types/errorRespose';
+import modalAction from '../modal/actions';
+import { TweetType, PostTweetType } from '../../types/tweet';
+import { ErrorResponse } from '../../types/errorResponse';
 
 const actionCreator = actionCreatorFactory();
 
@@ -12,11 +13,10 @@ const tweetAction = {
   getTweetList: actionCreator.async<{}, TweetType[], Error>(
     ActionTypes.getTweetList
   ),
+  postTweet: actionCreator.async<{}, {}, Error>(ActionTypes.postTweet),
 };
 
-export const fetchTweetList = () => async (
-  dispatch: Dispatch
-): Promise<void> => {
+export const fetchTweetList = () => async (dispatch: Dispatch) => {
   dispatch(tweetAction.getTweetList.started({ params: {} }));
   const tweetAPI = new TweetAPI();
   try {
@@ -25,7 +25,28 @@ export const fetchTweetList = () => async (
       const result = (await res.json()) as TweetType[];
       dispatch(tweetAction.getTweetList.done({ result, params: {} }));
     } else {
-      const result = (await res.json()) as ErrorRespose;
+      const result = (await res.json()) as ErrorResponse;
+      throw new Error(result.massage);
+    }
+  } catch (err) {
+    const error = err as Error;
+    dispatch(tweetAction.getTweetList.failed({ error, params: {} }));
+  }
+};
+
+export const fetchPostTweet = (data: PostTweetType) => async (
+  dispatch: Dispatch
+): Promise<any> => {
+  dispatch(tweetAction.postTweet.started({ params: {} }));
+  const tweetAPI = new TweetAPI();
+  try {
+    const res = await tweetAPI.postTweet(data);
+    if (res.ok) {
+      const result = await res.json();
+      dispatch(tweetAction.postTweet.done({ result, params: {} }));
+      dispatch(modalAction.setIsDisplay({ isDisplay: false }));
+    } else {
+      const result = (await res.json()) as ErrorResponse;
       throw new Error(result.massage);
     }
   } catch (err) {

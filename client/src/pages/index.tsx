43b7ff76323abe38f628748ae12d1280d4448a5f
junkisai <jonki.beam@gmail.com>
2020-04-15 13:07:23 +0900

    feat: Layout Wrapper

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
 import { NextPage } from 'next';
+import Head from 'next/head';
+import React from 'react';
+
+import withLayout from '../components/Layout';
 
 const Index: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>teamO Twitter</title>
+      </Head>
       <h1>Hello world!</h1>
     </>
   );
 };
 
-export default Index;
+export default withLayout(Index);

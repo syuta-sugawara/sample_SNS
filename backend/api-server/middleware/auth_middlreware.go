322c65104f4b535174f17979fb1b63f99c20f58b
DragonTaro1031 <dragon.taro.lioil@gmail.com>
2020-04-16 15:49:52 +0900

    feat:ラーハンドリング

--- backend/api-server/middleware/auth_middlreware.go
+++ backend/api-server/middleware/auth_middlreware.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"backend/api-server/controller"
 	"backend/api-server/domain/services"
+	"net/http"
 
 	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/labstack/echo"
@@ -12,16 +14,18 @@ func AuthMiddleware(auth *cognito.CognitoIdentityProvider) echo.MiddlewareFunc {
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			token, _ := c.Cookie("token")
+			token, err := c.Cookie("token")
+			if err != nil {
+				return c.JSON(http.StatusUnauthorized, controller.CreateErrorMessage("signin is reuiered"))
+			}
+
 			userID, err := userServices.VerifyUserOnCognito(token.Value)
 			if err != nil {
-				return err
+				return c.JSON(http.StatusUnauthorized, controller.CreateErrorMessage("token is invalid"))
 			}
 
 			c.Set("userID", *userID)
-
 			return next(c)
 		}
 	}
-
 }

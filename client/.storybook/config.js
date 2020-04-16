a072c449e3a50238ffd912e9b40de02f560c9680
junkisai <jonki.beam@gmail.com>
2020-04-16 18:17:50 +0900

    fix: font-family指定

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -5,6 +5,9 @@ import reset from '../src/styles/reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  body {
+    font-family: Arial, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, メイリオ, Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+  }
 `;
 
 configure(require.context('./stories', true, /\.stories\.tsx?$/), module);

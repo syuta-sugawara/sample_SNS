3a6092042c2001f1ad41a93ee92d4911892a7e52
junkisai <jonki.beam@gmail.com>
2020-04-20 19:53:32 +0900

    feat: フォローアンフォローリクエスト

--- client/src/requests/user.ts
+++ client/src/requests/user.ts
@@ -2,7 +2,26 @@ import fetch from 'isomorphic-unfetch';
 
 export default class UserAPI {
   readonly reqUrl = `${process.env.API_URL}/user`;
+
   getUser = (uid: string) => {
     return fetch(`${this.reqUrl}/${uid}`);
+  };
+
+  postFollow = (uid: string) => {
+    return fetch(`${this.reqUrl}/${uid}/follow`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    });
+  };
+
+  deleteFollow = (uid: string) => {
+    return fetch(`${this.reqUrl}/${uid}/follow`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    });
   };
 }

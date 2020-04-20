54e5b3e33a211598a27500a0a99b77c8fc03f2e3
junkisai <jonki.beam@gmail.com>
2020-04-20 19:45:57 +0900

    refactor: 共通部分一本化

--- client/src/pages/users/[uid]/index.tsx
+++ client/src/pages/users/[uid]/index.tsx
@@ -1,43 +1,29 @@
 import { NextPage } from 'next';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import styled from 'styled-components';
-
 import withLayout from '../../../components/Layout/';
-import withUserPageLayout from '../../../components/Layout/UserPage';
+import UserPageLayout from '../../../components/Layout/UserPage';
 import TweetItem from '../../../components/TweetItem';
-import STYLES from '../../../styles/const';
 import { RootState } from '../../../store';
 import { fetchTweetList } from '../../../store/tweet/actions';
+import { fetchUser } from '../../../store/user/actions';
 import { TweetType } from '../../../types/tweet';
 
 const User: NextPage = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const tweetList = useSelector((state: RootState) => state.tweet.results);
-  const uid = 'dummy';
-  console.log(router.query.uid);
+  const uid = router.query.uid as string;
+
   useEffect(() => {
+    dispatch(fetchUser(uid));
     dispatch(fetchTweetList());
   }, []);
 
   return (
-    <>
-      <TabList>
-        <TabItem primary>
-          <Link href="/users/[uid]" as={`/users/${uid}`}>
-            <a>ツイート</a>
-          </Link>
-        </TabItem>
-        <TabItem>
-          <Link href="/users/[uid]/likes" as={`/users/${uid}/likes`}>
-            <a>いいね</a>
-          </Link>
-        </TabItem>
-      </TabList>
+    <UserPageLayout>
       <ul>
         {tweetList.map((item: TweetType) => (
           <li key={item.id}>
@@ -45,48 +31,8 @@ const User: NextPage = () => {
           </li>
         ))}
       </ul>
-    </>
+    </UserPageLayout>
   );
 };
 
-const TabList = styled.nav`
-  display: flex;
-`;
-
-type TabItemProps = {
-  primary?: boolean;
-};
-
-const TabItem = styled.div`
-  position: relative;
-  flex: 1;
-  height: 60px;
-  font-size: 18px;
-  border-bottom-color: ${(props: TabItemProps): string =>
-    !props.primary ? STYLES.COLOR.GRAY_LIGHTER_20 : STYLES.COLOR.PRIMARY};
-  border-bottom-style: solid;
-  border-bottom-width: ${(props: TabItemProps): string =>
-    !props.primary ? '1px' : '2px'};
-  a {
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    color: ${(props: TabItemProps): string =>
-      !props.primary ? STYLES.COLOR.GRAY : STYLES.COLOR.PRIMARY};
-    text-decoration: none;
-  }
-  &:hover {
-    cursor: pointer;
-    background-color: ${STYLES.COLOR.PRIMARY_LIGHTER_30};
-    a {
-      color: ${STYLES.COLOR.PRIMARY};
-    }
-  }
-`;
-
-export default withLayout(withUserPageLayout(User));
+export default withLayout(User);

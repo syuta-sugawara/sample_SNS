9a9b08ea74dbce2540976a59d06205d2f256f8b0
DragonTaro1031 <dragon.taro.lioil@gmail.com>
2020-04-22 10:54:52 +0900

    fix:tokenに変更

--- backend/api-server/middleware/auth_middlreware.go
+++ backend/api-server/middleware/auth_middlreware.go
@@ -3,7 +3,9 @@ package middleware
 import (
 	"backend/api-server/controller"
 	"backend/api-server/domain/services"
+	"errors"
 	"net/http"
+	"strings"
 
 	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/labstack/echo"
@@ -14,12 +16,13 @@ func AuthMiddleware(auth *cognito.CognitoIdentityProvider) echo.MiddlewareFunc {
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			token, err := c.Cookie("token")
+			authHeader := c.Request().Header.Get("Authorization")
+			token, err := getToken(authHeader)
 			if err != nil {
 				return c.JSON(http.StatusUnauthorized, controller.CreateErrorMessage("signin is reuiered"))
 			}
 
-			userID, err := userServices.VerifyUserOnCognito(token.Value)
+			userID, err := userServices.VerifyUserOnCognito(token)
 			if err != nil {
 				return c.JSON(http.StatusUnauthorized, controller.CreateErrorMessage("token is invalid"))
 			}
@@ -28,4 +31,13 @@ func AuthMiddleware(auth *cognito.CognitoIdentityProvider) echo.MiddlewareFunc {
 			return next(c)
 		}
 	}
+}
+
+func getToken(header string) (string, error) {
+	bearerToken := strings.Split(header, " ")
+	if len(bearerToken) != 2 {
+		return "", errors.New("no authorization token found")
+	}
+
+	return bearerToken[1], nil
 }
